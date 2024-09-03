b97e637fd014034beb909466d8ac61e8bd46bae6
Adi <adityasajoo@gmail.com>
2024-09-04 01:06:58 +0930

    feat: add sidebar and navbar

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import React from "react";
+
+type Props = {};
+
+const Navbar = (props: Props) => {
+  return <div className="flex items-center gap-x-4 p-5 bg-green-300">Navbar</div>;
+};
+
+export default Navbar;
---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Sidebar = (props: Props) => {
+  return <div className="fixed z-[1] left-0 bg-blue-950 h-full w-[60px] flex p-3 flex-col gap-y-4 text-white">Sidebar</div>;
+};
+
+export default Sidebar;
